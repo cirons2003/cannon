71b0d36aae3a95d488ea841c11e00eb4af824393
Carson Irons <ci7229@princeton.edu>
2024-09-29 19:05:37 -0400

    hid password

--- cloud-server/app/__init__.py
+++ cloud-server/app/__init__.py
@@ -33,7 +33,7 @@ def create_app():
     app.config['MAIL_PORT'] = 587
     app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USERNAME'] = 'carsonirons@gmail.com'
-    app.config['MAIL_PASSWORD'] = 'qtrh xrom qvil pnmy'
+    app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
     app.config['MAIL_DEFAULT_SENDER'] = ('Cannon Club', 'carsonirons@gmail.com')
 
     #initialize extensions
